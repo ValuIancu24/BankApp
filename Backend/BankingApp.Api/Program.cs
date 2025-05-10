955b5bb0db2304a5d6afb59d5949bdefe1e4efce
matei1998 <mateiserband@gmail.com>
2025-05-10 15:34:20 +0300

    fix-workflows
    fixed workflows

--- Backend/BankingApp.Api/Program.cs
+++ Backend/BankingApp.Api/Program.cs
@@ -21,7 +21,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -35,6 +35,7 @@ app.MapGet("/weatherforecast", () =>
 
 app.Run();
 
+// Define the WeatherForecast record outside of the top-level statements
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
